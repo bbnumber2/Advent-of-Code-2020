9fb3972a81b3e35392882f39fabd52990428e380
bbnumber2 <caleb.m.brinkman@gmail.com>
2020-12-04 14:50:07 -0600

    Update day_4.py

--- day_4.py
+++ day_4.py
@@ -30,7 +30,7 @@ def main(puzzle_input):
         for passport in puzzle_input:
             #Try/except to not count failed int conversions
             try:
-                if (passport.keys() >= {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}) and \
+                if (passport.keys() >= {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}) and\
                 (1920 <= int(passport['byr']) <= 2002 and 2010 <= int(passport['iyr']) <= 2020 and 2020 <= int(passport['eyr']) <= 2030) and\
                 ((passport['hgt'][-2:] == 'cm' and 150 <= int(passport['hgt'][:-2]) <= 193) or (passport['hgt'][-2:] == 'in' and 59 <= int(passport['hgt'][:-2]) <= 76)) and\
                 (passport['hcl'][0] == '#' and len(passport['hcl'][1:]) == 6 and all(char in hair_colors for char in passport['hcl'][1:])) and\
